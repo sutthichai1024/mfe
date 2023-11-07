137389b89e2bf4f200a48cf1255d84c3231654c6
sutthichai.p <you@example.com>
2023-11-07 23:15:00 +0700

    Created container deploy script#8

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/marketingApp";
 export default () => {
   return (
     <div>
-      <h1> HI CONTAINER GIT HUB TEST</h1>
+      <h1> HI CONTAINER GIT HUB TEST 2</h1>
       <hr />
       <MarketingApp />
     </div>
