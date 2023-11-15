c08244606aac286c70f6a5becb349a0b1cfcf2ea
sutthichai.p <you@example.com>
2023-11-15 22:51:34 +0700

    add validation step to build 5

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/marketingApp";
 export default () => {
   return (
     <div>
-      <h1> HI CONTAINER GIT HUB TEST 4</h1>
+      <h1> HI CONTAINER GIT HUB TEST 5</h1>
       <hr />
       <MarketingApp />
     </div>
